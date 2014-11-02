f1bf808c7a80054a9275730410847b32c5d97c60
Ken Snyder <kendsnyder@gmail.com>
2014-11-01 22:27:31 -0600

    ready for demo

--- app/HostQuestions/HostQuestionsCtrl.js
+++ app/HostQuestions/HostQuestionsCtrl.js
@@ -3,4 +3,5 @@ app.controller("HostQuestionsCtrl", function HostQuestionsCtrl($scope, $firebase
 	var ref = new Firebase("https://brilliant-torch-8319.firebaseio.com/progress");
 	var sync = $firebase(ref);
 	$scope.progress = sync.$asObject();
+	
 });
